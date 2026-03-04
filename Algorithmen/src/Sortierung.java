04a098740180fbc0db2b3e569f2ad096e319a661
Leon Weimann <leonweimann@icloud.com>
2026-03-04 11:58:52 +0100

    04.03.2026

--- Algorithmen/src/Sortierung.java
+++ Algorithmen/src/Sortierung.java
@@ -2,10 +2,13 @@ import java.util.ArrayList;
 
 public class Sortierung {
     public static void main(String[] args) {
-        int arr[] = { 3, 2, 9, 1 };
+        int arr[] = {43, 20, 10, 12, 60, 50, 17, 80, 93, 54, 67, 18, 25};
+//        int arr[] = { 3, 2, 9, 1 };
         printArray(arr);
 
-        mergeSort(arr);
+//        mergeSort(arr);
+
+        quicksortTimed(arr);
 
         printArray(arr);
     }
@@ -15,11 +18,11 @@ public class Sortierung {
      *
      * [2, 3, 9, 1] // 3 steigt solange auf, bis eine größere Zahl (9) kommt
      * [2, 3, 1, 9] // im selben Schritt steigt dann die 9 auf
-     * 
+     *
      * [2, 1, 3, 9]
-     * 
+     *
      * [1, 2, 3, 9] // 2 steigt auf
-     * 
+     *
      * Ende: [1, 2, 3, 9]
      */
     public static void bubbleSort(int[] arr) {
@@ -44,7 +47,7 @@ public class Sortierung {
 
     /*
      * [8, 5, 9, 2, 1, 3] <- Eingabe
-     * 
+     *
      * [1,|5, 9, 2, 8, 3] <- 1 <=> 8
      * [1, 2,|9, 5, 8, 3] <- 2 <=> 5
      * ...
@@ -78,7 +81,7 @@ public class Sortierung {
      * [8, 5, 9][2, 1, 3]
      * [8][5, 9][2][1, 3]
      * ...[5][9]...[1][3]
-     * 
+     *
      * MERGE:
      * [8][5, 9][2][1, 3]
      * [5, 8, 9][1, 2, 3]
@@ -129,6 +132,105 @@ public class Sortierung {
         for (int x = l; x <= r; x++) {
             arr[x] = copy[x - l]; // x - l, da wir in copy von 0 zählen, in arr aber ab l (Anfang links)
         }
+    }
+
+    /*
+        Start = [43, 20, 10, 12, 60, 50, 17, 80, 93, 54, 67, 18, 25]
+        1.  Pivot = 43
+            <43: [20, 10, 12, 17, 18, 25]
+            >43: [60, 50, 80, 93, 54, 67]
+            =>:  [20, 10, 12, 17, 18, 25] 43 [60, 50, 80, 93, 54, 67]
+
+        2. Pivot = 20 (linker Teil von 1.) [20, 10, 12, 17, 18, 25]
+            <20: [10, 12, 17, 18]
+            >20: [25]
+            =>:  [10, 12, 17, 18] 20 [25]
+
+        3. Pivot = 10 (linken Teil von 2.) [10, 12, 17, 18]
+            <10: []
+            >10: [12, 17, 18]
+            =>:  10 [12, 17, 18]
+
+        4. Pivot = 12 (rechter Teil von 3.) [12, 17, 18]
+            <12: []
+            >12: [17, 18]
+            =>:  12 [17, 18]
+
+        5. Pivot = 17 (rechter Teil von 4.) [17, 18]
+            <17: []
+            >17: [18]
+            =>:  17 [18]
+            ==>: [10, 12, 17, 18, 20, 25]
+
+        6. Pivot = 60 (rechter Teil von 1.) [60, 50, 80, 93, 54, 67]
+            <60: [50, 54]
+            >60: [80, 93, 67]
+            =>:  [50, 54] 60 [80, 93, 67]
+
+        7. Pivot = 50 (linker Teil von 6.) [50, 54]
+            <50: []
+            >50: [54]
+            =>:  50 [54]
+
+        8. Pivot = 80 (rechter Teil von 6.) [80, 93, 67]
+            <80: [67]
+            >80: [93]
+            =>:  [67] 80 [93]
+            ==>: [50, 54, 60, 67, 80, 93]
+
+        Ende = [10, 12, 17, 18, 20, 25, 50, 54, 60, 67, 80, 93]
+
+        ---
+
+        Pivot:
+        Hier teilen wir unser zu sortierendes Array in zwei Teile auf.
+        Er kann beliebig in jedem Schritt gewählt werden. Hier der Einfachheit wegen
+        immer das erste Element des Teilarrays.
+
+        Eigenschaften:
+        Durchschnittliche Laufzeit: O(n log n)
+        Worst Case: O(n^2) (z.B. wenn Pivot immer genau in der Mitte der Zahlenwerte liegt)
+
+        Speicherbedarf: O(log n) durch Rekursion
+
+        Wichtig:
+        Die Wahl des Pivot ist wichtig dafür, ob Worst-Case eintritt. Man könnte
+        das Pivot Element immer zufällig wählen, jedoch:
+        - dauert jeder Schritt länger
+            - Mehr Schritte die weniger Zeit benötigen, gehen deutlich schneller
+        - insgesamt langsamer als feste Position
+
+        Mit Random Pivot: 0,00011..
+        Mit festem Pivot: 0,000004375...
+        => Random ist ca. >25x langsamer als festes Pivot
+     */
+    public static void quicksort(int[] arr, int l, int r) {
+        if (l >= r)
+            return;
+
+        int pivot = arr[r];
+        int i = l - 1;
+
+        for (int j = l; j < r; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                tauschen(arr, i, j);
+            }
+        }
+        tauschen(arr, i + 1, r);
+
+        quicksort(arr, l, i);
+        quicksort(arr, i + 1, r);
+    }
+
+    public static void quicksortTimed(int[] arr) {
+        double start = System.nanoTime();
+
+        quicksort(arr, 0, arr.length - 1);
+
+        double end = System.nanoTime();
+        double duration = (end - start) / 1000000000;
+        System.out.println("Dauer: " + duration + " Sekunden");
     }
 
     // --- Hilfszeugs ---
